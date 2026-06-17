051fb1645d31e244f84b527100fa03490f4df45d
Baraat Kahimalaev <155937234+Baraat04@users.noreply.github.com>
2026-06-17 14:54:24 +0500

    Update contact information and legal address

--- frontend/app/offer/page.tsx
+++ frontend/app/offer/page.tsx
@@ -213,8 +213,8 @@ export default function OfferPage() {
                 <strong>Наименование ИП/ЮЛ:</strong> ТОО &quot;SAAMA GROUP&quot;<br />
                 <strong>ИИН/БИН:</strong> 171040010072<br />
                 <strong>Расчетный счет (KZT):</strong> KZ87722S000021139717 (АО &quot;Kaspi Bank&quot;, БИК: CASPKZKA)<br />
-                <strong>Почта/телефон для обращений:</strong> geducation1017@gmail.com / +7 777 420-19-89<br />
-                <strong>Юридический адрес:</strong> 140000, Республика Казахстан, г. Павлодар, улица Малахова, дом 11
+                <strong>Почта/телефон для обращений:</strong> geducation1017@gmail.com / +7 706 430-71-95<br />
+                <strong>Юридический адрес:</strong> 140000, Республика Казахстан, г. Павлодар, улица Едыге Би, 71Б
               </p>
             </>
           )}
